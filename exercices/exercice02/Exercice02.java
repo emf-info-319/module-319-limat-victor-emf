06e0c55697a16ac690b23b6bba030978e36e3355
limat-victor-emf <victor.limat@studentfr.ch>
2024-08-29 10:53:43 +0200

    Exercice

---
+++ exercices/exercice02/Exercice02.java
@@ -0,0 +1,15 @@
+package exercices.exercice02;
+
+/**
+ * Exercice02
+ */
+public class Exercice02 {
+
+    public static void main(String[] args) {
+        // début de mon code
+        System.out.println("Bonjour tout le monde");
+        
+
+
+    }
+}
