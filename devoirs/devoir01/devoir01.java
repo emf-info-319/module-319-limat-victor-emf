ddd1e7c21cbaee10e2c06a19287122ed981e7124
limat-victor-emf <victor.limat@studentfr.ch>
2024-09-10 21:50:03 +0200

    devoir

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,21 @@
+package devoirs.devoir01;
+
+/**
+ * devoir01
+ */
+public class devoir01 {
+    public static void main(String[] args) {
+        int monAge = 16;
+        boolean estMajeur;
+ 
+        if (monAge > 20) {estMajeur = true;}
+        else {estMajeur = false;}
+        System.out.println("Je m'appelle Limat Victor");
+        System.out.println("Mon âge est de " + monAge + "ans");
+        if (estMajeur) {System.out.println("Je suis majeur");}
+        else {System.out.println("Je ne suis pas majeur");}
+    
+}
+}
+
+   
