8438a8b5521a17a1e93b6b71d5079a53c453c84e
limat-victor-emf <victor.limat@studentfr.ch>
2024-09-26 10:05:31 +0200

    Eval Formative

---
+++ revisions/LimatE00.java
@@ -0,0 +1,37 @@
+import javax.swing.Spring;
+
+public class LimatE00 {
+    static int NOMBRE_PTS_VICTOIRE = 3;
+
+    public static void main(String[] args) {
+        String nomEquipe1 = "Barcelon";
+        String nomEquipe2 = "Madrid";
+        int nombrePointEquipe1 = 0;
+        int nombrePointEquipe2 = 0;
+        int MAX = 9;
+        int MIN = 0;
+            
+        for (int i =0; i < 10; i++)
+           { int scoreEquipe1 = (int) (Math.random() * (MAX - MIN) + MIN);
+            int scoreEquipe2 = (int) (Math.random() * (MAX - MIN) + MIN);
+            System.out.println(
+                    nomEquipe1 + " joue contre " + nomEquipe2 + " et le résultat est : " + scoreEquipe1 + " à "
+                            + scoreEquipe2);
+            if (scoreEquipe1 > scoreEquipe2) {
+                nombrePointEquipe1 += NOMBRE_PTS_VICTOIRE;
+                System.out.println(nomEquipe1 + " a gagné");
+            }
+            if (scoreEquipe1 < scoreEquipe2) {
+                nombrePointEquipe2 += NOMBRE_PTS_VICTOIRE;
+                System.out.println(nomEquipe2 + " a gagné");
+            } if (scoreEquipe1 == scoreEquipe2) { 
+                System.out.println("Match nul");
+                nombrePointEquipe2 += 1;
+                nombrePointEquipe1 += 1;
+            } 
+        
+        System.out.println(nomEquipe1 + " a " + nombrePointEquipe1 + " points");
+        System.out.println(nomEquipe2 + " a " + nombrePointEquipe2 + " points"); }
+
+    }
+}
