846707fcaa0b7ffe74983d24bfb6b0bc37756d3d
limat-victor-emf <victor.limat@studentfr.ch>
2024-10-08 21:02:29 +0200

    devoir04

---
+++ exercices/exercice09/test.java
@@ -0,0 +1,11 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int compteur = 10;
+        while(compteur >= 0)
+        { System.err.println(compteur--);}
+    }
+}
