f3718060e309091066bd8afec111ee8b641137f0
limat-victor-emf <victor.limat@studentfr.ch>
2024-11-03 21:14:38 +0100

    Ex 10

--- exercices/exercice10/ExercicesMethodesTableaux.java
+++ exercices/exercice10/ExercicesMethodesTableaux.java
@@ -3,23 +3,83 @@
  */
 public class ExercicesMethodesTableaux {
 
+    //méthode main
     public static void main(String[] args) {
         int[] newTab = creerTab(3);
-        int[] newTab2 = remplirTab(newTab, 5);
+        int[] newTab2 = remplirTab(newTab, 6);
+        int tailleNewTab = retournerTailleTab(newTab);
+        int minTab = retournerMinTab(newTab);
+        int maxTab = retournerMaxTab(newTab);
+        int nbDefFoisValeur = retournerNbFoisValeurTab(newTab, 4);
+        int sommeTotalValeursTab = retournerSommeValeursTab(newTab);
     }
 
     //méthode qui créer un tableau
     public static int[] creerTab(int taille) {
-        int[] newTableau = new int[taille];
-        return newTableau;
+        int[] tab = new int[taille];
+        return tab;
     }
 
-    //méthode qui remplit un tableau
-    public static int[] remplirTab(int[] newTab, int valeur) {
-    for (int i = 0; i < newTab.length; i++) {
-        newTab[i] = valeur;
-    }
-    return newTab;    
+    //méthode qui remplit un tableau avec des valeurs aléatoires
+    public static int[] remplirTab(int[] tab, int valeur) {
+    for (int i = 0; i < tab.length; i++) {
+        tab[i] = valeur; }
+        return tab;
     }
 
+    //méthode qui retourne la taille du tableau
+    public static int retournerTailleTab(int[] tab) {
+        return tab.length;
+    }
+
+    //méthode qui affiche le contenu d'un tableau
+    public static void afficherContenuTab(int[] tab, String[] args) {
+        for (int i = 0; i < tab.length; i++) {
+            System.out.println( tab[i] ); 
+        }
+    }
+
+    //méthode qui recherche la valeur minimum d'un tableau
+    public static int retournerMinTab(int[] tab) {
+        if (tab.length == 0) {
+            return 0;
+        }
+        int min = tab[0];
+        for (int i = 1; i < tab.length; i++) {
+            min = Math.min(min, tab[i]);
+        }
+        return min;
+    }
+
+    //méthode qui recherche la valeur maximum d'un tableau
+    public static int retournerMaxTab(int[] tab) {
+        if (tab.length == 0) {
+            return 0;
+        }
+        int max =  tab[0];
+        for (int i = 1; i < tab.length; i++) {
+            max = Math.max(max, tab[i]);
+        }
+        return max;
+    }
+    //méthode qui compte la fréquenced'une valeur dans un tableau
+    public static int retournerNbFoisValeurTab(int[] tab, int valeur) {
+        int nbFois = 0;
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] == valeur) { nbFois++;
+                
+            }
+        }
+        return nbFois;
+    }
+
+    //méthode qui établit la somme des valeurs contenues dans un tableau
+    public static int retournerSommeValeursTab(int[] tab) {
+        int sommeValeurs = 0;
+        for (int i = 0; i < tab.length; i++) {
+            sommeValeurs += tab[i];
+        }
+        return sommeValeurs;
+    }
+    
 }
